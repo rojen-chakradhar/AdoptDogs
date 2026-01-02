b3c8f70f810400a79be31ce768660dc4ce7481ad
Rojen Chakradhar <chakradhar@rojen.me>
2026-01-03 00:30:38 +0545

    header changed in all files

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,20 @@ setTimeout(() => {
 	loader.style.top = "-100dvh";
 }, 1000);
 
+function show() {
+	let overlay =  document.querySelector("#overlay");
+	let notification =  document.querySelector("#notification");
+	overlay.style.display = "block";
+	notification.style.display = "flex";
+}
+
+function hide() {
+	let overlay =  document.querySelector("#overlay");
+	let notification =  document.querySelector("#notification");
+	overlay.style.display = "none";
+	notification.style.display = "none";
+}
+
 function edit() {
 	let editPage =  document.querySelector("#profile-edit");
 	let showPage =  document.querySelector("#profile-show");
